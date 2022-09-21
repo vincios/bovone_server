68832a658cddbe546491d2dffb93627cc55bc60c
Vincenzo <vinciosdev@gmail.com>
2022-09-21 15:51:51 +0200

    Added server port to the config

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     debug: bool = False
     data_csv_file_path: str | None
+    server_port: int | None
 
     class Config:
         env_file = "debug.env" if os.environ.get("BOVONE_DEBUG", False) else ".env"
