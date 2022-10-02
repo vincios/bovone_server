979635b4ebf01dcf78b839a9f70b69a8147958d8
Vincenzo <vinciosdev@gmail.com>
2022-10-02 21:12:13 +0200

    Added uvicorn server_host setting

--- app/config.py
+++ app/config.py
@@ -5,8 +5,9 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     debug: bool = False
+    server_host: str = "127.0.0.1"
+    server_port: int = 8000
     data_csv_file_path: str | None
-    server_port: int | None
 
     class Config:
         env_file = "debug.env" if os.environ.get("BOVONE_DEBUG", False) else ".env"
